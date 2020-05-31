5640feac6c9a7da107d0daf480509ae3dcf72429
Patrick Hulce <patrick.hulce@gmail.com>
2020-05-31 14:00:23 -0500

    feat: add basic logging

--- packages/electron/src/main/entry.ts
+++ packages/electron/src/main/entry.ts
@@ -1,6 +1,10 @@
 import {app, BrowserWindow} from 'electron'
+import {createLogger} from '../../../shared/src/utils/logging'
+
+const log = createLogger('electron:main')
 
 app.once('ready', () => {
+  log.info('app is ready')
   const window = new BrowserWindow({
     show: false,
     minWidth: 600,
@@ -8,5 +12,8 @@ app.once('ready', () => {
   })
 
   window.loadURL('data:text/html;<!DOCTYPE html><h1>Hello World')
-  window.once('ready-to-show', () => window.show())
+  window.once('ready-to-show', () => {
+    log.info('app is visible')
+    window.show()
+  })
 })
